bfb5b6ba638b9e145c5f1e4ab0efc77f58cad339
Riccardo Zanella <riccardo.zanella@gmail.com>
2025-12-28 12:01:17 +0100

    fix: solving vscode pytest error config

--- tests/test_download.py
+++ tests/test_download.py
@@ -25,7 +25,7 @@ def test_maybe_download(
     exists_mock = mocker.patch("os.path.exists")
     requests_get_mock = mocker.patch("requests.get")
     requests_get_mock.return_value.iter_content.return_value = [b"chunk"]
-    open_mock = mocker.patch("builtins.open", mocker.mock_open())
+    open_mock = mocker.patch("timeseries_autoencoder.data.open", mocker.mock_open())
 
     exists_mock.return_value = exists
     data.__maybe_download(link, filename, force=force)
